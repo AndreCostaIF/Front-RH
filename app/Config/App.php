2ae30c050771ce11d20edead4931a28a5982f6e0
Andre <miqueias.costa@escolar.ifrn.edu.br>
2022-03-15 17:33:40 -0300

    alterado caminho js

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://pirh.herokuapp.com/';
+    public $baseURL = 'https://pirh.herokuapp.com/public/';
 
     /**
      * --------------------------------------------------------------------------
